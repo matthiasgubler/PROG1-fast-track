7009ca917d63c0f1bd500e268bf07ed4ce4d5e25
Matthias Gubler (C766623) <matthias.gubler@axa-winterthur.ch>
2017-09-29 08:08:44 +0200

    Initial Commit: General Classes Body

---
+++ java/main/src/ch/mgubler/zhaw/SnakeGame.java
@@ -0,0 +1,26 @@
+package ch.mgubler.zhaw;
+
+public class SnakeGame {
+
+    public static final int SCREEN_WIDTH = 20;
+
+    public static final int SCREEN_HEIGHT = 20;
+
+    private Screen gameScreen;
+
+    public static void main(String[] args) {
+        SnakeGame snakeGame = new SnakeGame();
+        snakeGame.startGame();
+    }
+
+    public SnakeGame(){
+
+    }
+
+    public void startGame(){
+
+    }
+
+
+
+}
